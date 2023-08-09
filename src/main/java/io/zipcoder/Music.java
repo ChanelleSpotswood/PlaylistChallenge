c22f14a2175d679f9331c31c74daf84ae61753a4
ChanelleSpotswood <chanelle@zipcodersmbp103.myfiosgateway.com>
2023-08-09 19:22:02 -0400

    Playlist Challenge complete

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,51 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        Integer upIndex = 0;
+        Integer downIndex = 0;
+        Integer upNum = 0;
+        Integer downNum = 0;
+
+
+
+        // Set up the index
+        for (int i = (playList.length - 1); i >= 0; i--){
+            if (playList[i] == selection){
+                upIndex = i;
+                break;
+            }
+        }
+
+        // Set down the index
+        for (int i = 0; i < playList.length; i++)
+            if (playList[i] == selection){
+                downIndex = i;
+                break;
+            }
+
+        // Set up the num
+        if (startIndex == upIndex){
+            return 0;
+        }
+        else if (startIndex > upIndex){
+            upNum = upIndex - startIndex;
+        }
+        else {
+            upNum = (playList.length - upIndex) + startIndex;
+        }
+
+        // Set down the num
+        if (startIndex < downIndex){
+            downNum = downIndex - startIndex;
+        }
+        else {
+            downNum = (playList.length - downIndex) + startIndex;
+        }
+
+        if (upNum < downNum){
+            return upNum;
+        }
+        else return downNum;
+
     }
 }
